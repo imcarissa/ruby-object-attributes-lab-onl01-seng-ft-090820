c96caa1d8ecd985743b512a466ebf34c7eb36a11
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-19 13:27:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,3 @@
 class Dog
  
-  def initialize(name, breed)
-    @name = name
-    @name = name
-  end
- 
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
-  end
- 
-  def name
-    "#{@first_name} #{@last_name}".strip
-  end
- 
-end
+  
