9a36ff78e224a536bdc09148012c38a24a952b27
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-19 13:11:12 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+
+fido = Dog.new
+fido.name = "Fido"
+
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def name
+    @breed
+  end
+end
